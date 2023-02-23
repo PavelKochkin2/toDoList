35aeeeccd1cb7ad913ceba4bd12d7f7999bebbd5
Pavel Kochkin <pavkochk@publicisgroupe.net>
2023-02-23 19:48:21 +0100

    worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './App.css';
-import { ToDoList } from './ToDoList';
+import { ToDoList, Task } from './ToDoList';
 
 function App() {
+
+  let initTasks: Array<Task> = [
+    {id:1, name:"learn react", isDone: false},
+    {id:2, name:"learn .net", isDone: true},
+    {id:3, name:"learn azure", isDone: false},
+  ]
+
+
+
+ let arr =  useState(initTasks);
+ let tasks = arr[0];
+ let setTasks = arr[1];
+
+ 
+function removeTask(id: number){
+  let filteredTasks = tasks.filter(task => task.id !== id);
+  setTasks(filteredTasks);
+}
   return (
-    <ToDoList title='baby' tasks={[]} />
+    <ToDoList title='baby' tasks={tasks} removeTask={removeTask} />
   );
 }
 
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,4 @@
-type Task = {
+export type Task = {
   id: number,
   name: string,
   isDone: boolean
@@ -7,12 +7,24 @@ type Task = {
 export type PropsType = {
   title:string,
   tasks: Array<Task> 
+  removeTask: Function;
 }
 
 export function ToDoList(props: PropsType) {
   return (
     <div>
       <h1>{props.title}</h1>
+      {props.tasks.map(t=>{
+        return (
+          <ul>
+            <li>name: {t.name}</li>
+            <input type='checkbox' checked={t.isDone}/>
+            <input type='button' value='x' onClick={()=> {
+              props.removeTask(t.id)
+            }}/>
+          </ul>
+        )
+      })}
     </div>
   );
 }
